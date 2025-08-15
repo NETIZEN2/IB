bf29dc453c0ffd6ca24168eb756f0715fd52cc47
NETIZEN2 <jacob.tyler.wiremu@hotmail.com>
2025-08-15 12:41:33 +1000

    refactor: use lit-html for layout rendering

--- src/components/layout/AnalysisPanel.js
+++ src/components/layout/AnalysisPanel.js
@@ -1,5 +1,8 @@
+import { html } from 'lit-html';
+
 export function renderAnalysisPanel() {
-  return `
-    <div id="analysis-panel" class="w-1/2 brutalist-pane flex flex-col h-full overflow-y-auto"></div>
-  `;
+  return html`<div
+    id="analysis-panel"
+    class="w-1/2 brutalist-pane flex flex-col h-full overflow-y-auto"
+  ></div>`;
 }
